87a7cdd2ca39c9f73d3f73c815eeb272ea65fd70
Vladimir Revyagin <skorabeyf@gmail.com>
2024-06-26 14:07:09 +0300

    chore(.tsx):Понаставил костылей any

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react'
 
 export default function useLocalstorage(defaultValueKey: string) {
-  const [token, setToken] = useState<string>(defaultValueKey)
+  const [token, setToken] = useState<string | null | any>(defaultValueKey)
 
   useEffect(() => {
-    setToken(JSON.parse(localStorage.getItem(defaultValueKey)))
-  }, [token])
+    const item = localStorage.getItem(defaultValueKey)
+    setToken(item ? JSON.parse(item) : null)
+  }, [defaultValueKey, token])
 
-  function setItem(initialState: string) {
+  function setItem(initialState: string): void {
     return setToken(
       localStorage.setItem(defaultValueKey, JSON.stringify(initialState))
     )
   }
 
-  function removeItem() {
-    return setToken(localStorage.clear() || '')
+  function removeItem(): void {
+    const removeItem: any = localStorage.clear()
+    return setToken(removeItem ? removeItem : null)
   }
 
   return [token, { setItem, removeItem }]
